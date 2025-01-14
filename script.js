38c0f894f79ba724ef8e5ef78d91b6f40ab67261
YoungJin <oyj921205@gmail.com>
2025-01-14 21:59:12 +0900

    Add Modal windows

--- script.js
+++ script.js
@@ -15,45 +15,27 @@ const options = {
 
 let movieList = [];
 
+
 const fetchMovies = async function (url) {
   try { // try 블록 내 코드가 먼저 실행되고 이 안에서 예외가 발생하면 catch 블록 코드가 실행된다.
     const res = await fetch(url, options) //fetch 실행
     const data = await res.json(); // 응답 데이터를 JSON으로 변환
     return movieList = data.results // JSON으로 변환하고 받은 results값을 movieList 곳에 할당한다.
-   
+    
   } catch (err) {
     console.error(err);
   }
 }
-// function fetchMovies (url) {
-//   fetch(url, options)
-//   .then(function (res) {
-//     return res.json();
-//   })
-//   .then(function (movieList) {
-//     displayMovies(movieList);
-//   })
-// }
-
-//* --- HTML 화면에 보여줄 displayMovies 함수 실행 --- *//
-fetchMovies(popularUrl).then(function (movieList) {
-  displayMovies(movieList);
-});
-
 
 // * --- 받아온 데이터를 html에 카드로 구현하는 함수--- *//
 function displayMovies(movieList) {
   let html = "";
   movieList.forEach(function (movie) {
     html += `
-    <div class="card">
-      <div id="modal" class="modal">
-        <img src= "https://image.tmdb.org/t/p/w500/${movie.poster_path}"/>
-        <div>
-          <h2 class="card-title">${movie.title}</h2>
-            <p>평점 : ${movie.vote_average}</p>
-        </div>
-      </div>
+    <div class="card" data-movie="${movie.id}">
+      <img src= "https://image.tmdb.org/t/p/w500/${movie.poster_path}"/>
+      <h2 class="card-title">${movie.title}</h2>
+      <p>평점 : ${movie.vote_average}</p>
     </div>
     `;
   });
@@ -61,18 +43,61 @@ function displayMovies(movieList) {
 }
 
 //* --- 검색 기능 구현 --- *//
-  searchBtn.addEventListener("click", function () {
-    const keyword = searchInput.value; // 사용자가 입력한 검색어
-    const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=false&language=ko&page=1`
-   
-    fetchMovies(searchUrl).then(function (movieList) {
-      // 검색된 영화들 중에서 제목에 검색어가 포함된 영화들만 필터링
-      const filteredMovies = movieList.filter(function (movie) {
-        return movie.title.includes(keyword);  // 제목에 검색어가 포함되어 있는지 확인
-      });
-      
-      // 필터링된 결과를 화면에 표시
-      displayMovies(filteredMovies);
-  });
+searchBtn.addEventListener("click", function () {
+  const keyword = searchInput.value; // 사용자가 입력한 검색어
+  const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=false&language=ko&page=1`
+ 
+  fetchMovies(searchUrl).then(function (movieList) {
+    // 검색된 영화들 중에서 제목에 검색어가 포함된 영화들만 필터링
+    const filteredMovies = movieList.filter(function (movie) {
+      return movie.title.includes(keyword);  // 제목에 검색어가 포함되어 있는지 확인
+    });
+    
+    // 필터링된 결과를 화면에 표시
+    displayMovies(filteredMovies);
+
+    if (keyword === 0) {
+      displayMovies(movieList);
+    }
+});
 });
 
+//* --- 모달 기능 구현 --- *//
+document.querySelector("#cards").addEventListener("click", function (e) {
+  //! 예외처리
+  if (e.target.closest("div") && e.target.closest("div").classList.contains("card")) {
+    const movieId = e.target.closest(".card").getAttribute("data-movie")
+  modal.style.display = "block";
+  openModal(movieList, movieId);
+  }
+})
+
+//* --- 모달 열기 --- *//
+function openModal(movieList, movieId) {
+  const modal = document.querySelector("#modal");
+  let matchedMovie = movieList.filter(function (movie) {
+    if (movie.id == movieId) {
+      return movie;
+    }
+  })
+  const modalCard = document.querySelector(".modal-content");
+
+ let html = `
+          <img id="modal-img" src="https://image.tmdb.org/t/p/w500/${matchedMovie[0].poster_path}" alt="Movie Poster">
+          <h2 id="modal-title">${matchedMovie[0].title}</h2>
+          <p>${matchedMovie[0].overview}</p>
+          <p id="modal-rating">평점 : ${matchedMovie[0].vote_average}</p>
+ `;
+  modalCard.innerHTML = html;
+  modal.style.display = "block";
+}
+
+//* --- 모달 닫기 --- *//
+document.querySelector(".close").addEventListener("click", function () {
+  document.querySelector("#modal").style.display = "none";
+});
+
+//* --- HTML 화면에 보여줄 displayMovies 함수 실행 --- *//
+fetchMovies(popularUrl).then(function (movieList) {
+  displayMovies(movieList);
+});
