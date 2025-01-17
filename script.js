94c6b776e663beb91040be78adce513648133307
YoungJin <oyj921205@gmail.com>
2025-01-17 11:11:12 +0900

    Add API 코드분리

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
-const popularUrl = 'https://api.themoviedb.org/3/movie/popular?language=ko&page=1';
-const totalUrl = 'https://api.themoviedb.org/3/search/movie?query=%EC%95%88%EB%85%95&include_adult=false&language=ko&page=1';
-const movieCard = document.querySelector("#cards");
-const searchBtn = document.querySelector("#search-btn");
-const searchInput = document.querySelector("#search");
+import { popularUrl, totalUrl, movieCard, searchBtn, searchInput } from "./constant/constant.js"
+import { options, movieList ,fetchMovies } from "./APIs/api.js"
 
-//* --- 영화 데이터 불러오기 --- *//
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Njc0ZmEyN2QyMDcwNWQzOGMyZmM2YjhlNzE5YTgyOSIsIm5iZiI6MTczNjI5NjYzNC45NzksInN1YiI6IjY3N2RjOGJhMzRhNGU3NWU0OTdhZjcwOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.d6sU7icVuWpbcMVGPDwHEInLmc5DkpTr0vv88g8jgEI'
-  }
-};
-
-let movieList = [];
-
-
-const fetchMovies = async function (url) {
-  try { // try 블록 내 코드가 먼저 실행되고 이 안에서 예외가 발생하면 catch 블록 코드가 실행된다.
-    const res = await fetch(url, options) //fetch 실행
-    const data = await res.json(); // 응답 데이터를 JSON으로 변환
-    return movieList = data.results // JSON으로 변환하고 받은 results값을 movieList 곳에 할당한다.
-    
-  } catch (err) {
-    console.error(err);
-  }
+//* --- HTML 화면에 보여줄 initMovies 함수 실행 --- *//
+function initMovies() {
+  fetchMovies(popularUrl).then(function (movieList) {
+    console.log(movieList);
+    displayMovies(movieList);
+  });
 }
+
+initMovies();
 
 // * --- 받아온 데이터를 html에 카드로 구현하는 함수--- *//
 function displayMovies(movieList) {
@@ -93,11 +77,9 @@ function openModal(movieList, movieId) {
 }
 
 //* --- 모달 닫기 --- *//
-document.querySelector(".close").addEventListener("click", function () {
-  document.querySelector("#modal").style.display = "none";
-});
-
-//* --- HTML 화면에 보여줄 displayMovies 함수 실행 --- *//
-fetchMovies(popularUrl).then(function (movieList) {
-  displayMovies(movieList);
-});
+function closeModal () {
+  document.querySelector(".close").addEventListener("click", function () {
+      document.querySelector("#modal").style.display = "none";
+    });
+}
+closeModal();
