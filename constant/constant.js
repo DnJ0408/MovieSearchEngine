2dc8a00752fe56360e171e6dd2e72ac57d63bcde
YoungJin <oyj921205@gmail.com>
2025-01-17 11:11:53 +0900

    Add constant 코드 분리

---
+++ constant/constant.js
@@ -0,0 +1,5 @@
+export const popularUrl = 'https://api.themoviedb.org/3/movie/popular?language=ko&page=1';
+export const totalUrl = 'https://api.themoviedb.org/3/search/movie?query=%EC%95%88%EB%85%95&include_adult=false&language=ko&page=1';
+export const movieCard = document.querySelector("#cards");
+export const searchBtn = document.querySelector("#search-btn");
+export const searchInput = document.querySelector("#search");
